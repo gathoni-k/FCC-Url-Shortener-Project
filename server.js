7ebd6e3a224a08e0df1fbfe322c551fbc8341834
gathoni-k <maryg.kahiga@gmail.com>
2020-09-10 04:17:58 +0300

    Create routes

--- server.js
+++ server.js
@@ -8,16 +8,22 @@ var cors = require('cors')
 
 var app = express()
 
+const dotenv = require('dotenv')
+const { shortenUrl, getLongUrl } = require('./controllers/url.controllers')
+dotenv.config()
+
 // Basic Configuration
 var port = process.env.PORT || 3000
 
 /** this project needs a db !! **/
-// mongoose.connect(process.env.DB_URI);
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(e => console.log(e))
 
 app.use(cors())
-
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/public', express.static(process.cwd() + '/public'))
 
@@ -29,6 +35,9 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' })
 })
+
+app.post('/api/shorturl/new', shortenUrl)
+app.get('/api/shorturl/:shortcode', getLongUrl)
 
 app.listen(port, function () {
   console.log('Node.js listening ...')
